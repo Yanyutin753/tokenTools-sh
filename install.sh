135c9d7f2c6e6dbcbf383de8ece78490ec98d2e0
Yanyutin753 <3254822118@qq.com>
2023-11-24 22:45:13 +0800

    新增脚本

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 安装 Docker
+sudo apt-get install docker-ce
+
+# 切换到根目录
+cd /
+
+# 安装 Git
+sudo apt update
+sudo apt install git
+
+# 克隆 GitHub 仓库
+git clone https://github.com/Yanyutin753/most-simply-deploy-PandoaraNext.git /pandora
+
+
+# 进入 /pandora 目录
+cd /pandora
+
+# 运行 Docker Compose 启动命令
+echo "启动 开启8081端口和8181端口"
+docker-compose up -d
