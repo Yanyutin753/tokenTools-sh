51009048b99eae404a825da1b0753edcc0df997c
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2023-11-25 22:22:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
-# 安装 Docker
-sudo apt update
-sudo apt-get install docker-ce
+# 检查是否已经安装 docker-ce
+if ! command -v docker &> /dev/null
+then
+    # 安装 Docker
+    sudo apt update
+    sudo apt-get install docker-ce
+else
+    echo "Docker 已经安装，跳过安装步骤。"
+fi
+
+# 检查是否已经安装 docker-compose
+if ! command -v docker-compose &> /dev/null
+then
+    # 安装 docker-compose
+    sudo apt install docker-compose
+else
+    echo "Docker Compose 已经安装，跳过安装步骤。"
+fi
 
 # 切换到根目录
 cd /
 
-# 安装 docker-compose
-sudo apt install docker-compose
+# 设置仓库地址和目标目录
+repo_url="git clone https://github.com/Yanyutin753/most-simply-deploy-PandoaraNext.git"
+target_dir="/pandora"
 
-# 克隆 GitHub 仓库
-git clone https://github.com/Yanyutin753/most-simply-deploy-PandoaraNext.git /pandora
+# 检查目标目录是否已经存在
+if [ -d "$target_dir" ]; then
+    echo "目标目录已存在，将进行删除全部目录并拉取最新代码操作。"
+    # 切换到其他目录，然后删除目标目录下所有内容
+    cd / && rm -rf "$target_dir"
+fi
 
+# 克隆 Gitee 仓库到目标目录并切换到主分支
+git clone "$repo_url" "$target_dir" && cd "$target_dir" && git checkout main
 
 # 进入 /pandora 目录
 cd /pandora
