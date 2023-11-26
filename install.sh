57ad05c886e323df5b4a7c294195761fc3e81c39
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2023-11-26 12:54:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
-
-# 检查是否已经安装 docker-ce
+echo "检查是否已经安装 docker-ce"
 if ! command -v docker &> /dev/null
 then
     # 安装 Docker
     sudo apt update
     sudo apt-get install docker-ce
+    echo "安装Docker成功！"
 else
     echo "Docker 已经安装，跳过安装步骤。"
 fi
 
-# 检查是否已经安装 docker-compose
+echo "检查是否已经安装 docker-compose"
 if ! command -v docker-compose &> /dev/null
 then
     # 安装 docker-compose
     sudo apt install docker-compose
+    echo "安装docker-compose成功！"
 else
     echo "Docker Compose 已经安装，跳过安装步骤。"
 fi
@@ -33,12 +33,13 @@ if [ -d "$target_dir" ]; then
     cd / && rm -rf "$target_dir"
 fi
 
-# 克隆 Gitee 仓库到目标目录并切换到主分支
+echo "克隆 github 仓库到目标目录并切换到主分支"
 git clone https://github.com/Yanyutin753/most-simply-deploy-PandoaraNext.git /pandora
+echo "克隆成功"
 
-# 进入 /pandora 目录
+echo "进入 /pandora 目录"
 cd /pandora
 
 # 运行 Docker Compose 启动命令
-echo "启动 开启8081端口和8181端口"
 docker-compose up -d
+echo "启动成功 确保开启8081端口和8181端口"
