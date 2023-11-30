57f37a76cabe49dd3d8ae5685b7c47b6e35b722b
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2023-11-30 19:40:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,53 @@
+#!/bin/bash
+
+# 函数：确认删除目录
+confirm_delete() {
+    echo "目标目录已存在，是否删除并拉取最新代码？(y/n)"
+    read confirm
+    if [ "$confirm" == "y" ]; then
+        # 检查是否有足够的权限
+        if [ ! -w /pandora ]; then
+            echo "没有足够的权限删除目录，请检查权限设置。"
+            exit 1
+        fi
+
+        # 删除目录的操作
+        cd / && rm -rf /pandora
+    else
+        echo "取消操作，退出脚本。"
+        exit 1
+    fi
+}
+
 echo "检查是否已经安装 docker"
-if ! command -v docker &> /dev/null
-then
+if ! command -v docker &> /dev/null; then
     # 安装 Docker
     sudo apt update
     sudo apt-get install docker-ce
-    echo "安装Docker成功！"
+    echo "安装 Docker 成功！"
 else
     echo "Docker 已经安装，跳过安装步骤。"
 fi
 
 echo "检查是否已经安装 docker-compose"
-if ! command -v docker-compose &> /dev/null
-then
+if ! command -v docker-compose &> /dev/null; then
     # 安装 docker-compose
     sudo apt install docker-compose
-    echo "安装docker-compose成功！"
+    echo "安装 docker-compose 成功！"
 else
     echo "Docker Compose 已经安装，跳过安装步骤。"
 fi
-
-# 切换到根目录
-cd /
 
 # 设置目标目录
 target_dir="/pandora"
 
 # 检查目标目录是否已经存在
 if [ -d "$target_dir" ]; then
-    echo "目标目录已存在，将进行删除全部目录并拉取最新代码操作。"
-    # 切换到其他目录，然后删除目标目录下所有内容
-    cd / && rm -rf "$target_dir"
+    confirm_delete
 fi
 
 echo "克隆 github 仓库到目标目录并切换到主分支"
-git clone https://github.com/Yanyutin753/most-simply-deploy-PandoaraNext.git /pandora
+git clone https://gitee.com/yangyangEN/most-simply-deploy-PandoaraNext.git /pandora
 echo "克隆成功"
 
 echo "进入 /pandora 目录"
@@ -41,4 +55,4 @@ cd /pandora
 
 # 运行 Docker Compose 启动命令
 docker-compose up -d
-echo "启动成功 确保开启8081端口和8181端口"
+echo "启动成功，请确保开启 8081 端口和 8181 端口"
