b7daf8a322cdd87c2802b4c3204ec8e415c85482
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2023-11-30 19:41:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ if [ -d "$target_dir" ]; then
 fi
 
 echo "克隆 github 仓库到目标目录并切换到主分支"
-git clone https://gitee.com/yangyangEN/most-simply-deploy-PandoaraNext.git /pandora
+git clone https://github.com/Yanyutin753/most-simply-deploy-PandoaraNext.git /pandora
 echo "克隆成功"
 
 echo "进入 /pandora 目录"
