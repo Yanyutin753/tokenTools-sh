c5a1d9cba49a4c38d3c7b32cfd9786e3baf97856
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2023-12-27 23:29:04 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,49 +1,55 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "检查 /pandora 路径是否存在"
 if [ -d "/pandora" ]; then
     echo "开始更新 Pandora 服务..."
 
     # 切换到 /pandora 目录
-    cd /pandora
+    cd /pandora || exit
 
     # 停止 tokensTool 和 PandoraNext 服务
-    docker stop tokensTool PandoraNext
+    docker-compose down
 
-    # 移除 tokensTool 和 PandoraNext 服务
-    docker rm -f tokensTool PandoraNext
-
-    echo 拉取最新的镜像
+    echo "拉取最新的镜像"
     docker-compose pull
 
     # 重新构建并启动所有服务
     docker-compose up -d --build
     
-    running_images=$(docker ps --format "{{.Image}}")
+    running_images=$(docker ps -a --format "{{.Image}}")
     
-    # 获取名为PandoraNext和tokensTool的所有镜像
-    images=$(docker images --format "{{.ID}} {{.Repository}}" | grep -E 'PandoraNext|tokensTool')
+    # 获取所有镜像
+    all_images=$(docker images -a --format "{{.ID}} {{.Repository}}")
     
     # 初始化一个变量来跟踪是否所有的镜像都被成功删除
     all_images_removed=true
     
     # 遍历所有的镜像
-    while IFS= read -r image; do
-        # 提取镜像ID
-        image_id=${image%% *}
+    echo "$all_images" | while IFS= read -r image; do
+        # 提取镜像ID和镜像名
+        image_id=$(echo "$image" | cut -d' ' -f1)
+        image_name=$(echo "$image" | cut -d' ' -f2)
     
-        # 检查这个镜像是否正在运行
-        if ! grep -q "$image_id" <<<"$running_images"; then
-            # 如果这个镜像没有正在运行，那么删除它
-            docker rmi "$image_id"
-            
-            # 如果删除镜像的命令失败，那么设置 all_images_removed 为 false
-            if [ $? -ne 0 ]; then
-                all_images_removed=false
-            fi
-        fi
-    done <<<"$images"
-
+        # 检查这个镜像是否是 PandoraNext 或 tokensTool
+        case "$image_name" in
+            PandoraNext|tokensTool)
+                # 检查这个镜像是否正在运行
+                case "$running_images" in
+                    *"$image_name"*)
+                        ;; # Image is running, do nothing
+                    *)
+                        # 如果这个镜像没有正在运行，那么删除它
+                        docker rmi -f "$image_id"
+                        
+                        # 如果删除镜像的命令失败，那么设置 all_images_removed 为 false
+                        if [ $? -ne 0 ]; then
+                            all_images_removed=false
+                        fi
+                        ;;
+                esac
+                ;;
+        esac
+    done
 
     # 等待所有的后台任务都完成
     wait
@@ -52,7 +58,7 @@ if [ -d "/pandora" ]; then
     if $all_images_removed; then
         echo "旧镜像删除成功。"
     else
-        echo "旧镜像没有被成功删除。"
+        echo "有些镜像没有被成功删除。"
     fi
 else
     echo "没有 /pandora 路径，请先一键部署之后才能一键更新。"
